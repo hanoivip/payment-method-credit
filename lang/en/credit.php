26228a11d65146596fd74217047c69ac985c5d85
hanoivip <game.oh.vn@gmail.com>
2021-10-05 11:18:00 +0700

    g

---
+++ lang/en/credit.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'guide' => 'Use your credit point to pay. Points can be rewarded by out-game events or giftcode',
+    'type' => [
+        0 => 'Main balance',
+        1 => 'Acc balance'
+    ],
+    'failure' => [
+        'not-enough' => 'Credit point is not enough',
+        'fail-to-charge' => 'Payment failure. Plz try again.'
+    ]
+];
